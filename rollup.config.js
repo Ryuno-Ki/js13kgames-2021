33f2a8f0d013cc5495d6bf32f3164618a55c24fc
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-13 13:40:54 +0200

    chore: configure rollup
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import buble from '@rollup/plugin-buble'
+import copy from 'rollup-plugin-copy'
+import license from 'rollup-plugin-license'
+import { terser } from 'rollup-plugin-terser'
+
+const LICENSE_HEADER = `
+This file is part of JS13kGames - SPACE.
+<insert name here> is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+An Offline Life is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with An Offline Life.  If not, see <https://www.gnu.org/licenses/>.`
+
+export default {
+  input: './src/app.js',
+  output: {
+    file: './tmp/app.js',
+    format: 'iife',
+  },
+  plugins: [
+    copy({
+      flatten: true,
+      verbose: true,
+      targets: [{
+        src: './src/index.html',
+        dest: './dist/'
+      }]
+    }),
+
+    buble(),
+    terser({
+      output: {
+        comments: false
+      }
+    }),
+
+    license({
+      banner: LICENSE_HEADER
+    })
+  ]
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 
-import { app } from '../src/index.js'
+import { app } from '../src/app.js'
 
 describe('App', function () {
   it('should work', function () {

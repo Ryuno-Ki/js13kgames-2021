4fe39389e298bcc8cad9de33ab59c058ebab392f
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-14 22:12:32 +0200

    fix: set name for IIFE outputs
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- rollup.config.js
+++ rollup.config.js
@@ -9,18 +9,19 @@ This file is part of JS13kGames - SPACE.
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-An Offline Life is distributed in the hope that it will be useful,
+<insert name here> is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
-along with An Offline Life.  If not, see <https://www.gnu.org/licenses/>.`
+along with <insert name here>.  If not, see <https://www.gnu.org/licenses/>.`
 
 export default {
   input: './src/js/app.js',
   output: {
     file: './dist/js/app.js',
     format: 'iife',
+    name: 'game'
   },
   plugins: [
     copy({

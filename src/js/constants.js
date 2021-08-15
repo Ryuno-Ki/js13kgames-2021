ba978bc381f1e057859e0934c942dcfd35e8ba5c
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-15 13:59:43 +0200

    feat: declare FPS as constant
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,4 @@
 import { Vec2 } from './vector.js'
 
+export const FPS = 60
 export const gravity = Vec2(0, 100)
--- types/constants.d.ts
+++ types/constants.d.ts
@@ -1,1 +1,2 @@
+export const FPS: 60;
 export const gravity: import("./vector.js").Vector2D;

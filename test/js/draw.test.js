da945ee840e82b0ae5c69e5e52fa307fcf4cc014
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-17 19:32:25 +0200

    refactor: swap jsdom + canvas dep in favour of a mock object
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- test/js/draw.test.js
+++ test/js/draw.test.js
@@ -1,20 +1,13 @@
 import { expect } from 'chai'
-import jsdom from 'jsdom'
 
 import { drawShape } from '../../src/js/draw.js'
 import { RigidShape } from '../../src/js/shape.js'
 import { Vec2 } from '../../src/js/vector.js'
 
-// Requires npm canvas package here to be installed
-const { JSDOM } = jsdom
-const { window } = new JSDOM('')
-const { document } = window
-
 describe('drawShape', function () {
   it('should draw a shape onto a canvas', function () {
     // Arrange
-    const canvas = document.createElement('canvas')
-    const context = canvas.getContext('2d')
+    const context = makeContext()
     const center = Vec2(500, 200)
     const friction = 20
     const restitution = 0
@@ -40,3 +33,13 @@ describe('drawShape', function () {
     // TODO: Assert pixel on canvas
   })
 })
+
+function makeContext () {
+  return {
+    restore: function () {},
+    rotate: function () {},
+    save: function () {},
+    strokeRect: function () {},
+    translate: function () {},
+  }
+}

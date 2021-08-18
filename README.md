3f09b544d02fe5caff22431d6db42d274318234a
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-18 21:39:37 +0200

    docs: link to other documentation
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- README.md
+++ README.md
@@ -5,3 +5,19 @@ The topic of js13kgames 2021 is SPACE.
 You're an astronaut. Your spaceship suffered heavy damage so you had to escape
 with your spacesuit on. But the energy is running low. Find battery packs, but
 also keep an eye on your life support systems!
+
+## Game Design Document
+
+See [GDD][gdd].
+
+## Journal
+
+A daily journal can be found at [Journal][journal].
+
+## License
+
+GPL v3 or newer. See [LICENSE][license].
+
+[gdd]: ./gdd/
+[journal]: ./journal/
+[license]: ./LICENSE.txt
--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,17 @@ import { terser } from 'rollup-plugin-terser'
 
 const LICENSE_HEADER = `
 This file is part of JS13kGames - SPACE.
-<insert name here> is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-<insert name here> is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with <insert name here>.  If not, see <https://www.gnu.org/licenses/>.`
+Combat Scorched Earth from Outer Space is free software:
+you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+Combat Scorched Earth from Outer Space is distributed in the hope
+that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License
+along with Combat Scorched Earth from Outer Space.
+If not, see <https://www.gnu.org/licenses/>.`
 
 export default {
   input: './src/js/app.js',

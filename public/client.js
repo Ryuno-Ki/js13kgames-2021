24d7723ac785282f6427cc9e65e5a922fee5b507
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-21 23:24:01 +0200

    chore: track new generated code for deployment
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- public/client.js
+++ public/client.js
@@ -1,123 +1,15 @@
-"use strict";
+/**
+ * This file is part of JS13kGames - SPACE.
+ * Lido Space is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Lido Space is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Lido Space.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-(function () {
-
-    let socket, //Socket.IO client
-        buttons, //Button elements
-        message, //Message element
-        score, //Score element
-        points = { //Game points
-            draw: 0,
-            win: 0,
-            lose: 0
-        };
-
-    /**
-     * Disable all button
-     */
-    function disableButtons() {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].setAttribute("disabled", "disabled");
-        }
-    }
-
-    /**
-     * Enable all button
-     */
-    function enableButtons() {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].removeAttribute("disabled");
-        }
-    }
-
-    /**
-     * Set message text
-     * @param {string} text
-     */
-    function setMessage(text) {
-        message.innerHTML = text;
-    }
-
-    /**
-     * Set score text
-     * @param {string} text
-     */
-    function displayScore(text) {
-        score.innerHTML = [
-            "<h2>" + text + "</h2>",
-            "Won: " + points.win,
-            "Lost: " + points.lose,
-            "Draw: " + points.draw
-        ].join("<br>");
-    }
-
-    /**
-     * Binde Socket.IO and button events
-     */
-    function bind() {
-
-        socket.on("start", () => {
-            enableButtons();
-            setMessage("Round " + (points.win + points.lose + points.draw + 1));
-        });
-
-        socket.on("win", () => {
-            points.win++;
-            displayScore("You win!");
-        });
-
-        socket.on("lose", () => {
-            points.lose++;
-            displayScore("You lose!");
-        });
-
-        socket.on("draw", () => {
-            points.draw++;
-            displayScore("Draw!");
-        });
-
-        socket.on("end", () => {
-            disableButtons();
-            setMessage("Waiting for opponent...");
-        });
-
-        socket.on("connect", () => {
-            disableButtons();
-            setMessage("Waiting for opponent...");
-        });
-
-        socket.on("disconnect", () => {
-            disableButtons();
-            setMessage("Connection lost!");
-        });
-
-        socket.on("error", () => {
-            disableButtons();
-            setMessage("Connection error!");
-        });
-
-        for (let i = 0; i < buttons.length; i++) {
-            ((button, guess) => {
-                button.addEventListener("click", function (e) {
-                    disableButtons();
-                    socket.emit("guess", guess);
-                }, false);
-            })(buttons[i], i + 1);
-        }
-    }
-
-    /**
-     * Client module init
-     */
-    function init() {
-        socket = io({ upgrade: false, transports: ["websocket"] });
-        buttons = document.getElementsByTagName("button");
-        message = document.getElementById("message");
-        score = document.getElementById("score");
-        disableButtons();
-        bind();
-    }
-
-    window.addEventListener("load", init, false);
-
-})();
+var lido=function(n){"use strict";var o,t,e,i,r,u=!1,a=[];function c(){var n=document.getElementById("host");if(!n)throw new Error("Cannot start game!");t=n,a=[],e=window.io({upgrade:!1,transports:["websocket"]}),document.body.addEventListener("keydown",s,!1),document.body.addEventListener("keyup",f,!1),e.on("start",d),e.on("win",l),e.on("lose",p),e.on("end",v),e.on("connect",w),e.on("disconnect",h),e.on("error",g),i=(new Date).valueOf(),b()}function s(){u||(o=(new Date).valueOf(),u=!0)}function f(){var n=(r=(new Date).valueOf())-o;Number.isNaN(n/1e3)||(a.push((i-r)/1e3),b()),u=!1}function d(){m("Round ")}function l(){y()}function p(){y()}function v(){m("Waiting for opponent…")}function w(){m("Waiting for opponent…")}function h(){m("Connection lost!")}function g(){m("Connection error!")}function m(n){console.log(n)}function y(n){console.log(y)}function b(){var n,o,e,i,r,u=(n=[160,100],o=[].concat([n]),e=130,i=13,r=26,a.forEach((function(t){var r=n[0],u=n[1],a=function(n){var o=n.radius,t=n.angle;return{x:o*Math.cos(t),y:o*Math.sin(t)}}({radius:e*Math.random(),angle:E(360*t/i)}),c=a.x,s=a.y;o.push([r+c,u+s]),o.push([r,u])})),o.slice(-1*r)).map((function(n){return n.join(",")})).join(" ");t.setAttribute("points",u)}function E(n){return n*Math.PI/180}return n.app=function(){window.addEventListener("load",c,!1)},Object.defineProperty(n,"__esModule",{value:!0}),n}({});

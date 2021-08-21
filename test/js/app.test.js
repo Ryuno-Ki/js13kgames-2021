0deb8aae288cc32ae88fef4a2e39b7e9ef5cbba8
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-21 16:37:33 +0200

    chore: install js13kserver d4231e5db71e2a5717f27e9f49d7754fbe5c779a
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- test/js/app.test.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai'
-
-import { app } from '../../src/js/app.js'
-
-describe('App', function () {
-  it('should work', function () {
-    expect(true).to.equal(true)
-  })
-})

7b62303f7e89e6c660e0abdeada523e999b7b6ec
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-22 18:52:04 +0200

    fix: repair tests no longer logging to console
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- test/on-disconnect.mjs
+++ test/on-disconnect.mjs
@@ -1,18 +1,9 @@
 import assert from 'assert'
-import sinon from 'sinon'
 
 import { onDisconnect } from '../src/js/on-disconnect.js'
 
 describe('onDisconnect', () => {
-  let spy
-
-  beforeEach(() => spy = sinon.spy(console, 'log'))
-
-  afterEach(() => sinon.restore())
-
   it('should set a message', () => {
     onDisconnect()
-
-    assert(spy.calledOnce)
   })
 })
--- test/on-end.mjs
+++ test/on-end.mjs
@@ -1,18 +1,9 @@
 import assert from 'assert'
-import sinon from 'sinon'
 
 import { onEnd } from '../src/js/on-end.js'
 
 describe('onEnd', () => {
-  let spy
-
-  beforeEach(() => spy = sinon.spy(console, 'log'))
-
-  afterEach(() => sinon.restore())
-
   it('should set a message', () => {
     onEnd()
-
-    assert(spy.calledOnce)
   })
 })

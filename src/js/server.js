22c659b4c9d0524926f65138aeea4b4621dc447e
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-22 15:49:27 +0200

    feat: move server logic into dedicated file
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/server.js
@@ -0,0 +1,7 @@
+import { io } from './io.js'
+import { stat } from './stat.js'
+
+export {
+  io,
+  stat
+}
---
+++ types/server.d.ts
@@ -0,0 +1,3 @@
+import { io } from "./io.js";
+import { stat } from "./stat.js";
+export { io, stat };

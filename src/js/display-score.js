d6c18f91720b744c0b9abcd754af9092e69263cd
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-22 13:50:20 +0200

    feat: break displayScore out into dedicated file
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/display-score.js
@@ -0,0 +1,9 @@
+/**
+ * Shows the score.
+ *
+ * @deprecated
+ * @param {string} message
+ */
+export function displayScore (message) {
+  console.log(message)
+}
---
+++ types/display-score.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Shows the score.
+ *
+ * @deprecated
+ * @param {string} message
+ */
+export function displayScore(message: string): void;

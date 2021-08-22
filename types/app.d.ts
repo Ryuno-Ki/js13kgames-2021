977d5016c1ccedc0cfff3883b19dcfcc8b71864d
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-22 14:02:43 +0200

    refactor: use init from dedicated file
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- types/app.d.ts
+++ types/app.d.ts
@@ -2,11 +2,3 @@
  * Execute the client side JavaScript.
  */
 export function app(): void;
-export type cartesian = {
-    x: number;
-    y: number;
-};
-export type polar = {
-    radius: number;
-    angle: number;
-};

b2e434dfd897292b5e7d5ad58157912a0605e591
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-22 18:54:32 +0200

    refactor: replay synched events
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/bind.js
+++ src/js/bind.js
@@ -2,29 +2,37 @@ import { onConnect } from './on-connect.js'
 import { onDisconnect } from './on-disconnect.js'
 import { onEnd } from './on-end.js'
 import { onError } from './on-error.js'
-import { onKeyDown } from './on-key-down.js'
-import { onKeyUp } from './on-key-up.js'
 import { onLoose } from './on-loose.js'
 import { onStart } from './on-start.js'
 import { onWin } from './on-win.js'
 import { state } from './state.js'
+import { updateHost } from './update-host.js'
 
 /**
  * Bind to event listeners.
  */
 export function bind () {
-  document.body.addEventListener('keydown', onKeyDown, false)
-  document.body.addEventListener('keyup', onKeyUp, false)
-
   if (state.socket === null) {
     throw new Error('Game not properly initialised!')
   }
 
   state.socket.on('start', onStart)
+  state.socket.on('sync', onSync)
   state.socket.on('win', onWin)
   state.socket.on('lose', onLoose)
   state.socket.on('end', onEnd)
   state.socket.on('connect', onConnect)
   state.socket.on('disconnect', onDisconnect)
   state.socket.on('error', onError)
+}
+
+/**
+ * Reply synced state.
+ *
+ * @param {object} details
+ * @param {number} details.delta
+ */
+function onSync (details) {
+  state.points.push(details.delta)
+  updateHost()
 }

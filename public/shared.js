f1ab05de9515709db76eb2fb1034524a5c7eaad9
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-24 21:52:17 +0200

    chore: commit current state
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- public/shared.js
+++ public/shared.js
@@ -1,6 +1,11 @@
-'use strict';
+/* eslint-disable */
+'use strict'
 
+// @ts-ignore
 const ROLE_HOST = 'HOST'
+// @ts-ignore
 const ROLE_OPPONENT = 'OPPONENT'
+// @ts-ignore
 const ROLE_SPECTATOR = 'SPECTATOR'
+// @ts-ignore
 const ROLE_UNKNOWN = null

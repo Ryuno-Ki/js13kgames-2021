290bd9be074592d9e6e5da26ed15daa48f9730c7
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-28 18:59:07 +0200

    feat: helper function to navigate between scenes
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ src/js/client/navigate.js
@@ -0,0 +1,14 @@
+/**
+ * Update the history and move to new scene.
+ *
+ * @param {string} target
+ */
+export function navigate (target) {
+  const state = {}
+  const title = ''
+  const url = new URL(window.location.href)
+  url.hash = `#scene-${target}`
+
+  window.history.pushState(state, title, url.href)
+  window.location.hash = url.href
+}
---
+++ types/client/navigate.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Update the history and move to new scene.
+ *
+ * @param {string} target
+ */
+export function navigate(target: string): void;

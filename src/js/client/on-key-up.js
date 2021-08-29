7f7355315052fc969a9d84685e8a29b4f0a3881d
André Jaenisch <andre.jaenisch@posteo.de>
2021-08-29 22:58:28 +0200

    refactor: host SOCKET_KEY_UP only transmits the delta in seconds
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/client/on-key-up.js
+++ src/js/client/on-key-up.js
@@ -1,26 +1,16 @@
+import { SOCKET_KEY_UP } from '../constants.js'
 import { dom } from './dom.js'
 import { state } from './state.js'
-import { updateHost } from './update-host.js'
 
 /**
  * React on keyup events.
  */
 export function onKeyUp () {
   state.upTime = (new Date()).valueOf()
-  const delta = state.upTime - state.downTime
+  const delta = (state.upTime - state.downTime) / 1000
 
-  // TODO: Why not both?
-  if (!Number.isNaN(delta / 1000)) {
-    state.hostPoints.push((state.startTime - state.upTime) / 1000)
-    updateHost()
-    dom.socket.emit(
-      'keyUp',
-      {
-        delta: (state.startTime - state.upTime) / 1000,
-        position: null,
-        role: state.role
-      }
-    )
+  if (!Number.isNaN(delta)) {
+    dom.socket.emit(SOCKET_KEY_UP, { delta })
   }
 
   state.isPressed = false
